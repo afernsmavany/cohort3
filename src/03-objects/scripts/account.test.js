aea86be1efab5048b4f7b81b1af81214cf3dcb27
asif <mavany.asif@gmail.com>
2019-11-06 15:30:47 -0700

    Updated completed Comp 130A

--- src/03-objects/scripts/account.test.js
+++ src/03-objects/scripts/account.test.js
@@ -35,20 +35,3 @@ test('Check withdrawal', () => {
 
 
 
-// test('Check if accName works', () => {
-//     const checkingAccount = new Account("Checking Account", 25);
-//     expect(checkingAccount.deposit()).toEqual("Larry")
-//     expect(d.getName()).toEqual("Dean")
-//   });
-  
-  // test('Check if getAge works', () => {
-  //   const l = new Person("Larry", 5);
-  //   const d = new Person("Dean", 29);
-  //   expect(l.getAge()).toEqual(5);
-  //   l.getBirthday();
-  //   expect(l.getAge()).toEqual(6);
-    
-  //   expect(d.getAge()).toEqual(29);
-  //   d.getBirthday();
-  //   expect(d.getAge()).toEqual(30);
-  // });
