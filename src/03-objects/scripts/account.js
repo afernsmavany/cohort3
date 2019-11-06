8008c7a23f58789ad54681a8830df017981a8b4e
asif <mavany.asif@gmail.com>
2019-11-06 15:28:53 -0700

    Updated completed Comp 130A

--- src/03-objects/scripts/account.js
+++ src/03-objects/scripts/account.js
@@ -1,7 +1,30 @@
 
 class Account {
 
-  constructor() {
-    this.
+  constructor(accountName, balance) {
+    this.accountName = accountName;
+    this.balance = balance;
+  }
+
+  deposit(amount) {
+    this.balance += amount;
+  }
+
+  withdraw(amount) {
+    this.balance -= amount;
+  }
+
+  showBalance() {
+    return this.balance;
   }
 }
+
+class AccountController {
+
+  // constructor(accountName, balance) {
+  //   this.accountName = accountName;
+  //   this.balance = balance;
+  // }
+}
+
+export {Account, AccountController};
--- src/03-objects/scripts/account.test.js
+++ src/03-objects/scripts/account.test.js
@@ -1,8 +1,54 @@
 
-import functions from './account.js'
+import {Account, AccountController} from './account.js';
 
-test('Check balance', () => {
-    expect(functions.balance(25)).toEqual(25)
-    expect(functions.balance(35)).toEqual(35)
+
+test('Check constructor', () => {
+  const checkingAccount = new Account("Checking Account", 25);
+  expect(checkingAccount.accountName).toEqual("Checking Account")
+  expect(checkingAccount.balance).toEqual(25)
 });
 
+test('Check deposit', () => {
+  const checkingAccount = new Account("Checking Account", 25);
+  checkingAccount.deposit(10);
+  expect(checkingAccount.balance).toEqual(35);
+});
+
+test('Check withdrawal', () => {
+  const checkingAccount = new Account("Checking Account", 35);
+  checkingAccount.withdraw(30);
+  expect(checkingAccount.balance).toEqual(5);
+});
+
+test('Check withdrawal', () => {
+  const checkingAccount = new Account("Checking Account", 5);
+  checkingAccount.showBalance();
+  expect(checkingAccount.balance).toEqual(5);
+});
+
+
+
+
+
+
+
+
+
+
+// test('Check if accName works', () => {
+//     const checkingAccount = new Account("Checking Account", 25);
+//     expect(checkingAccount.deposit()).toEqual("Larry")
+//     expect(d.getName()).toEqual("Dean")
+//   });
+  
+  // test('Check if getAge works', () => {
+  //   const l = new Person("Larry", 5);
+  //   const d = new Person("Dean", 29);
+  //   expect(l.getAge()).toEqual(5);
+  //   l.getBirthday();
+  //   expect(l.getAge()).toEqual(6);
+    
+  //   expect(d.getAge()).toEqual(29);
+  //   d.getBirthday();
+  //   expect(d.getAge()).toEqual(30);
+  // });
