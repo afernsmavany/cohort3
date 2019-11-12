780e28ebd624f1b9ee590e704a313bd4cdc8f5f6
asif <mavany.asif@gmail.com>
2019-11-11 23:07:37 -0700

    Updated Ex. 130 C Account Controller

--- src/03-objects/scripts/account.js
+++ src/03-objects/scripts/account.js
@@ -21,10 +21,33 @@ class Account {
 
 class AccountController {
 
-  // constructor(accountName, balance) {
-  //   this.accountName = accountName;
-  //   this.balance = balance;
-  // }
+  constructor() {
+    this.accountList = [];
+  }
+
+  createAccount(accountName, balance) {
+    this.accountList.push(new Account(accountName, balance));
+  }
+
+  removeAccount(accountName) {
+    this.accountList = this.accountList.filter(account => {
+      account.accountName !== accountName
+    });
+  }
+
+  totalAccounts() {
+    return this.accountList.reduce(((acc, cur) => acc + cur.balance), 0);
+  }
+
+  highestAccount() {
+    let highestVal = this.accountList.reduce((a, b) => {return Math.max(a, b)});
+    return highestVal;
+  }
+
+  lowestAccount() {
+    return this.accountList.reduce((a, b) => {return Math.min(a, b)});
+  }
+
 }
 
-export {Account, AccountController};
+export { Account, AccountController };
--- src/03-objects/scripts/account.test.js
+++ src/03-objects/scripts/account.test.js
@@ -1,35 +1,62 @@
 
-import {Account, AccountController} from './account.js';
+import { Account, AccountController } from './account.js';
 
-
-test('Check constructor', () => {
+describe('Account Tests', () => {
   const checkingAccount = new Account("Checking Account", 25);
-  expect(checkingAccount.accountName).toEqual("Checking Account")
-  expect(checkingAccount.balance).toEqual(25)
+  test('Check Account', () => {
+    expect(checkingAccount.accountName).toEqual("Checking Account")
+    expect(checkingAccount.balance).toEqual(25)
+  });
+
+  test('Check deposit', () => {
+    checkingAccount.deposit(10);
+    expect(checkingAccount.balance).toEqual(35);
+  });
+
+  test('Check withdrawal', () => {
+    checkingAccount.withdraw(30);
+    expect(checkingAccount.balance).toEqual(5);
+  });
+
+  test('Check balance', () => {
+    checkingAccount.showBalance();
+    expect(checkingAccount.balance).toEqual(5);
+  });
 });
 
-test('Check deposit', () => {
-  const checkingAccount = new Account("Checking Account", 25);
-  checkingAccount.deposit(10);
-  expect(checkingAccount.balance).toEqual(35);
+describe('Account Controller Tests', () => {
+  const accountManager = new AccountController();
+
+  test('Check Account creation', () => {
+    accountManager.createAccount("Savings Account", 100)
+    expect(accountManager.accountList[0].accountName).toEqual("Savings Account");
+    expect(accountManager.accountList[0].balance).toEqual(100)
+  });
+
+  test('Check Account removal', () => {
+    accountManager.removeAccount("Savings Account")
+    expect(accountManager.accountList[0]).toEqual(undefined);
+    expect(accountManager.accountList).toEqual([]);
+  });
+
+  test('Check Account Total', () => {
+    accountManager.createAccount("Checking Account", 1000)
+    accountManager.createAccount("Savings Account", 2000)
+    expect(accountManager.totalAccounts()).toEqual(3000);
+  });
+
+  test('Check Account with Highest balance', () => {
+    accountManager.highestAccount();
+    expect(accountManager.highestAccount()).toEqual("Savings Account", 2000);
+  });
+
+  test('Check Account with Lowest balance', () => {
+    accountManager.lowestAccount();
+    expect(accountManager.lowestAccount()).toEqual("Checking Account", 1000);
+  });
+
+
 });
-
-test('Check withdrawal', () => {
-  const checkingAccount = new Account("Checking Account", 35);
-  checkingAccount.withdraw(30);
-  expect(checkingAccount.balance).toEqual(5);
-});
-
-test('Check withdrawal', () => {
-  const checkingAccount = new Account("Checking Account", 5);
-  checkingAccount.showBalance();
-  expect(checkingAccount.balance).toEqual(5);
-});
-
-
-
-
-
 
 
 
