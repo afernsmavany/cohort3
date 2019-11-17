218183d9bd26da03980dccecfacf8c809ee6c7c5
asif <mavany.asif@gmail.com>
2019-11-16 17:41:46 -0700

    Updated functionality of depositbutton

--- src/03-objects/scripts/domFuncs.js
+++ src/03-objects/scripts/domFuncs.js
@@ -8,20 +8,37 @@ const domFuncs = {
     newAcct.id = accountName;
     newAcct.className = "card";
     newAcct.textContent = accountName;
-    idLeftPanel.appendChild(newAcct);
-   
+
+
+    let newAcctInput = document.createElement("input");
+    // let newInputId = accountName + 'input';
+    // newAcctInput.setAttribute('id', newInputId);
+    newAcctInput.setAttribute('class', "inputField2");
+    newAcctInput.setAttribute("type", "text");
+    newAcctInput.placeholder = "Please enter amount..";
+    newAcct.appendChild(newAcctInput);
+
     let buttonDeposit = document.createElement("button");
-    buttonDeposit.id = idDeposit;
+    // let newDepositId = accountName + 'deposit';
+    buttonDeposit.setAttribute('class', "depositButton");
+    // buttonDeposit.setAttribute('id', newDepositId);
     buttonDeposit.textContent = "Deposit";
     newAcct.appendChild(buttonDeposit);
 
+
     let buttonWithdrawal = document.createElement("button");
+
     buttonWithdrawal.textContent = "Withdrawal";
     newAcct.appendChild(buttonWithdrawal);
 
+
     let buttonDelete = document.createElement("button");
+
     buttonDelete.textContent = "Delete Account";
     newAcct.appendChild(buttonDelete);
+
+    idLeftPanel.appendChild(newAcct);
+   
 
   }
 };
--- src/03-objects/scripts/main.js
+++ src/03-objects/scripts/main.js
@@ -2,6 +2,8 @@ import { Account, AccountController } from './account.js';
 import domFuncs from './domFuncs.js';
 
 const accountManager = new AccountController;
+// const userInput2 = document.getElementsByClassName("inputField2");
+const account = new Account;
 
 idAddBalance.addEventListener("click", () => {
     let accountName = (input1.value);
@@ -9,32 +11,36 @@ idAddBalance.addEventListener("click", () => {
     accountManager.createAccount(accountName, balance);
     domFuncs.createAccountCard(accountName, balance);
     console.log(accountManager.accountList);
+});
 
-    idDeposit.addEventListener("click", () => {
-        // let accountName = (input1.value);
-        // let balance = Number(input2.value);
-        accountManager.deposit(amount);
-        // domFuncs.createAccountCard(accountName, balance);
+idLeftPanel.addEventListener("click", (e) => {
+    if (e.target.className === "depositButton") {
+        let currentCard = e.toElement.parentElement;
+        let cardInputVal = Number(currentCard.children[1].value);
+        console.log(cardInputVal);
+        console.log(accountManager[0]);
+    //    accountManager[0].deposit(cardInputVal);
+        console.log("Hello Deposit");
         console.log(accountManager.accountList);
+    };
 
-
-        // idWithdrawal.addEventListener("click", () => {
-        //     let accountName = (input1.value);
-        //     let balance = Number(input2.value);
-        //     accountManager.createAccount(accountName, balance);
-        //     domFuncs.createAccountCard(accountName, balance);
-        //     console.log(accountManager.accountList);
-
-        //     idDelAcct.addEventListener("click", () => {
-        //         let accountName = (input1.value);
-        //         let balance = Number(input2.value);
-        //         accountManager.createAccount(accountName, balance);
-        //         domFuncs.createAccountCard(accountName, balance);
-        //         console.log(accountManager.accountList);
+});
 
 
 
-            });
+    // idWithdrawal.addEventListener("click", () => {
+    //     let accountName = (input1.value);
+    //     let balance = Number(input2.value);
+    //     accountManager.createAccount(accountName, balance);
+    //     console.log(accountManager.accountList);
+
+    //     idDelAcct.addEventListener("click", () => {
+    //         let accountName = (input1.value);
+    //         let balance = Number(input2.value);
+    //         accountManager.createAccount(accountName, balance);
+    //         console.log(accountManager.accountList);
+
+
 
 
 
