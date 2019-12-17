432c0dd22943bc4edb190a3fc442b6bdf6e99b1b
asif <mavany.asif@gmail.com>
2019-12-17 16:32:46 -0700

    Updated Comp 140 B tutorial: lifting state

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -5,6 +5,8 @@ import Xmas2 from './icons/Xmas2.svg';
 import Xmas3 from './icons/Xmas3.svg';
 import Xmas4 from './icons/Xmas4.svg';
 import './App.css';
+import Icon from './components/Icon.js';
+import { Board, Game, Square } from './components/tictactoe/Game.js';
 
 class App extends Component {
   constructor() {
@@ -12,28 +14,58 @@ class App extends Component {
     this.state = {
       selected: Xmas1
     }
+    this.style = { backgroundColor: '#d4bea7' };
+    this.icons = [Xmas1, Xmas2, Xmas3, Xmas4];
+  }
+
+  onSelect = (event) => {
+    this.setState({
+      selected: event.target.name
+    });
+  }
+
+  showPage = () => {
+    // if (this.state.selected === Xmas1) return <Starter logo={logo}/>;
+    if (this.state.selected === Xmas2) return <Game />;
+    // if (this.state.selected === bankIcon) return <AccountsApp />;
+    // if (this.state.selected === communityIcon) return <CitiesApp />;
+    // if (this.state.selected === linkIcon) return <LinkedListApp />;
+  }
+
+  renderIcons = () => {
+    return this.icons.map((icon) => {
+      return <Icon key={icon} name={icon} image={icon} style={this.state.selected === icon ? this.style : null}
+        onClick={this.onSelect} />
+    })
   }
 
   render() {
-  return(
-    <div className = "App" >
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    return (
+      <div className="App" >
+        <div>
+          <nav className="Nav">
+            {this.renderIcons()}
+          </nav>
+          {this.showPage()}
+        </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
-}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
