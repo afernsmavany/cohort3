99fe676c0f133cf380f063e075b89afed8cbd98c
asif <mavany.asif@gmail.com>
2020-01-03 20:53:26 -0700

    Updated 140 C Accounts basic UI

---
+++ react-02/src/components/accounts/accountPSC.js
@@ -0,0 +1,59 @@
+
+class Account {
+
+  constructor(accountName, balance) {
+    this.accountName = accountName;
+    this.balance = balance;
+  }
+
+  deposit (amount) {
+    this.balance += amount;
+  }
+
+  withdrawal(amount) {
+    this.balance -= amount;
+  }
+
+  showBalance() {
+    return this.balance;
+  }
+}
+
+class AccountController {
+
+  constructor() {
+    this.accountList = [];
+  }
+
+  createAccount(accountName, balance) {
+    const obj =new Account(accountName, balance)
+    this.accountList.push(obj);
+  }
+
+  // removeAccount(accountName) {
+  //   this.accountList = this.accountList.filter(account => {
+  //     account.accountName !== accountName
+  //   });
+  // }
+
+  totalAccounts() {
+    return this.accountList.reduce(((acc, cur) => acc + cur.balance), 0);
+  }
+
+  highestAccount() {
+    return this.accountList.sort((a, b) => b.balance - a.balance)[0];
+    // let highestVal = this.accountList.reduce((a, b) => {return Math.max(a, b)});
+    // return highestVal;
+  }
+
+  lowestAccount() {
+    return this.accountList.sort((a, b) => a.balance - b.balance)[0];
+  }
+
+}
+
+export { Account, AccountController };
+
+
+
+
--- react-02/src/components/accounts/accounts.js
+++ react-02/src/components/accounts/accounts.js
@@ -1,40 +1,77 @@
 import React, { Component } from 'react';
 import './accounts.css';
-
+import { AccountController } from './accountPSC.js';
 
 class Accounts extends Component {
-  // constructor() {
-  //   super()
-    
-  
+  constructor() {
+    super()
+    this.state = {
+      accountName: "",
+      accountBal: null,
+      AController: new AccountController()
+    }
+  }
+  // oldState =  () => {
+  //   this.setState({accountName : ""});
+  //   this.setState({accountBal : ""})
   // }
 
-render() {
-  return (
+  accountName = (eValue) => {
+    this.setState({ accountName: eValue })
+  }
 
-    <section id="cards">
-    <h2> My Accounts </h2>
-    <div id="idPanelContainer">
-        <div id="idLeftPanel" class="leftPanel">Account Manager
-            <input id="input1" type="text" placeholder="Add an Account Name"/>
-            <input id="input2" type="text" placeholder="Enter initial balance"/>
-            <button class="addBalance" id="idAddBalance">Create New Account</button>
-        </div>
+  accountBal = (eValue) => {
+    this.setState({ accountBal: eValue })
+  }
 
-        <div id="idRightPanel" class="rightPanel"> My Current Balances
-            <h3 id="display"></h3>
+  // handleClick = (() =>{this.setState(AController: AController.createAccount(this.state.accountName, this.state.accountBal));
+  handleClick = (() => {
+    const arrayAccounts = this.state.AController;
+    arrayAccounts.createAccount(this.state.accountName, this.state.accountBal);
+    // this.setState(this.state.AController.createAccount(this.state.accountName, this.state.accountBal));
+    this.setState({
+      AController: arrayAccounts,
+    })
+  }
+  )
+
+  // addAccountCard = (() => {
+  //   return (
+  //     <div className="card">
+  //       <h2 className="cardHeader">Current {this.state.accountName} {this.state.accountBal}
+  //       </h2> <input className="inputField2" type="text" placeholder="Please enter amount.." />
+  //       <button className="depositButton">Deposit</button>
+  //       <button className="withdrawalButton">Withdrawal</button>
+  //       <button className="deleteButton">Delete Account</button> <output class="outputBoxClass" id="outputBoxId"></output>
+  //     </div>)
+  // }
+  // )
+
+  render() {
+
+    return (
+      <div>
+        <div id="cards" />
+        <h2> My Accounts </h2>
+        <div id="idPanelContainer">
+          <div id="idLeftPanel" className="leftPanel">Account Manager
+            <input id="input1" type="text" placeholder="Add an Account Name" onBlur={(event) => this.accountName(event.target.value)} />
+            <input id="input2" type="text" placeholder="Enter initial balance" onBlur={(event) => this.accountBal(event.target.value)} />
+            <button className="addBalance" id="idAddBalance"
+              onClick={() => { this.handleClick() }}> Create New Account</button>
+
+            <div className="card">
+              <h2 className="cardHeader">Current {this.state.accountName} {this.state.accountBal} ></h2>
+                <button className="withdrawalButton">Withdrawal</button>
+                <button className="deleteButton">Delete Account</button> <output class="outputBoxClass" id="outputBoxId"></output>
+            </div>
       </div>
-      </div>
-   
-</section>
-
-
-
-
+          </div>
+          </div>
+          
+        ); 
+      }
+    }
     
-  );
-}
-}
-
-
+    
 export default Accounts; 
