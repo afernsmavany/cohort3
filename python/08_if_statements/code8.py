02fa436a8a6663cd574e0437067efc959d32f9c1
asif <mavany.asif@gmail.com>
2020-01-22 13:35:22 -0700

    Completed udemy booleans exercise

---
+++ python/08_if_statements/code8.py
@@ -0,0 +1,20 @@
+print(5 == 5)
+print(5 > 5)
+print(10 != 10)
+
+# Comparisons: ==, !=, >, <, >=, <=
+
+friends = ["Rolf", "Bob"]
+abroad = ["Rolf", "Bob"]
+
+print(friends == abroad)
+print(friends is abroad)
+print(friends is friends)
+
+#2:
+friends = ["Rolf", "Bob"]
+abroad = friends
+
+print(friends == abroad)
+print(friends is abroad)
+
