71bb97bba97dbe36ac01f16884ea4af2229ec31b
asif <mavany.asif@gmail.com>
2020-02-13 09:57:28 -0700

    Updated Ex.18 functions returning values

---
+++ python/18_functions_returning_values/code18.py
@@ -0,0 +1,17 @@
+# example (functions return none):
+
+def add(x, y):
+    print(x + y)
+
+add(5, 8)
+result = add(5, 8)
+print(result)
+
+# example 2 (functions return none):
+
+def add(x, y):
+    print(x + y)
+
+add(5, 8)
+result = add(5, 8)
+print(result)
